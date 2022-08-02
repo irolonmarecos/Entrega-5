b55a5029cbfebf883917830b3f6bed42a5410b1f
irolonmarecos <69371947+irolonmarecos@users.noreply.github.com>
2022-08-02 00:47:50 -0300

    EJS

---
+++ Ejs/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const router = require('./routes/account');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static( 'public'));
+app.use('/api/productos',router);
+app.set('view engine', 'ejs');
+
+
+
+const PORT = process.env.PORT || 2020
+const server = app.listen(PORT, ()=>{
+    console.log(`El servidor que se esta ejecutando es el ${PORT}`);
+})
+server.on("error", error => `Error: ${error}`);
